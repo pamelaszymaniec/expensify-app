97136577f4b840abd566bae44ea981ef9d1b3de3
Pamela Szymaniec <pamela.szymaniec@gmail.com>
2018-07-20 10:07:13 +0200

    task 10/88 ES6 Object destructuring

---
+++ src/playground/destructuring.js
@@ -0,0 +1,26 @@
+// const person = {
+//     name: 'Pamela',
+//     age: 25,
+//     location: {
+//         city: 'Gdansk',
+//         temp: 26
+//     }
+// };
+// const { name: firstName = 'Anonymous', age } = person;
+// console.log(`${firstName} is ${age}.`);
+// const { city, temp: temperature } = person.location;
+// if (city && temperature) {
+//     console.log(`It's ${temperature} in ${city}.`)
+// };
+
+const book = {
+    title: 'Ego is the Enemy',
+    author: 'Ryan Holiday',
+    publisher: {
+        name: 'Penguin'
+    }
+};
+
+const {name: publisherName = 'Self-Published'} = book.publisher;
+
+console.log(publisherName);
