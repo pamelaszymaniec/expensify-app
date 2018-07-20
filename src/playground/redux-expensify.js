05b591cd37b8bda5d4e450eef76c9c027f42f942
Pamela Szymaniec <pamela.szymaniec@gmail.com>
2018-07-20 12:50:48 +0200

    task 10/94 ES6 Spreading Objects

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,8 +1,7 @@
 import {createStore, combineReducers} from 'redux';
 import uuid from 'uuid';
 
-//ADD_EXPENSE
-
+// ADD_EXPENSE
 const addExpense = (
     {
         description = '',
@@ -20,24 +19,36 @@ const addExpense = (
         createdAt
     }
 });
-//REMOVE_EXPENSE
+
+// REMOVE_EXPENSE
 const removeExpense = ({id} = {}) => ({
     type: 'REMOVE_EXPENSE',
     id
 });
 
-//REMOVE_EXPENSE
-//EDIT_EXPENSE
-//SET_TEXT_FILTER
-//SORT_BY_DATE
-//SORT_BY_AMOUNT
-//SET_START_DATE
-//SET_END_DATE
+// EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+});
 
-//Expenses Reducer
+// SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+});
+
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_END_DATE
+
+// Expenses Reducer
+
 const expensesReducerDefaultState = [];
-const expensesReducer = (state = expensesReducerDefaultState, action) => {
 
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
     switch (action.type) {
         case 'ADD_EXPENSE':
             return [
@@ -45,16 +56,26 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
                 action.expense
             ];
         case 'REMOVE_EXPENSE':
-            return state.filter(({ id }) => id !== action.id);
-            // Metoda filter wykonuje dostarczoną funkcję callback dla
-        // każdego elementu tablicy, tworząc nową tablicę z wszystkich
-        // tych elementów, dla których funkcja callback zwróciła wartość true. Wynikowa tablica jest zwarta; wartości, które nie przechodzą testu funkcji callback, są zwyczajnie pomijane i nie są przypisywane do indeksów nowej tablicy.
+            return state.filter(({id}) => id !== action.id);
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if (expense.id === action.id) {
+                    return {
+                        ...expense,
+                        ...action.updates
+                    };
+                } else {
+                    return expense;
+                }
+                ;
+            });
         default:
             return state;
     }
 };
 
-//Filters Reducer
+// Filters Reducer
+
 const filtersReducerDefaultState = {
     text: '',
     sortBy: 'date',
@@ -64,6 +85,11 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
     switch (action.type) {
+        case 'SET_TEXT_FILTER':
+            return {
+                ...state,
+                text: action.text
+            };
         default:
             return state;
     }
@@ -79,21 +105,21 @@ const store = createStore(
 );
 
 store.subscribe(() => {
-    console.log(store.getState())
+    console.log(store.getState());
 });
 
 const expenseOne = store.dispatch(addExpense({description: 'Rent', amount: 100}));
-const expenseTwo = store.dispatch(addExpense({description: 'Coffe', amount: 30}));
-
-console.log(expenseOne);
-console.log(expenseTwo);
+const expenseTwo = store.dispatch(addExpense({description: 'Coffee', amount: 300}));
 
 store.dispatch(removeExpense({id: expenseOne.expense.id}));
+store.dispatch(editExpense(expenseTwo.expense.id, {amount: 500}));
 
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter());
 
 const demoState = {
     expenses: [{
-        id: 'dasdasd',
+        id: 'poijasdfhwer',
         description: 'January Rent',
         note: 'This was the final payment for that address',
         amount: 54500,
@@ -101,9 +127,8 @@ const demoState = {
     }],
     filters: {
         text: 'rent',
-        sortBy: 'amount', //date or amount
+        sortBy: 'amount', // date or amount
         startDate: undefined,
         endDate: undefined
     }
-
 };
