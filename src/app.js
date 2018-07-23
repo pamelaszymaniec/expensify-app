d08210d91f392869245f181ac43d94360b7d2378
Pamela Szymaniec <pamela.szymaniec@gmail.com>
2018-07-23 14:53:39 +0200

    task 11/101 Connecting Store and Component with React-Redux

--- src/app.js
+++ src/app.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 
-import { addExpense } from './actions/expenses'
-import { setTextFilter} from './actions/filters'
+import {addExpense} from './actions/expenses'
+import {setTextFilter} from './actions/filters'
 import getVisibleExpenses from './selectors/expenses'
-
 
 
 const store = configureStore();
 store.dispatch(addExpense({description: 'Water bill'}));
 store.dispatch(addExpense({description: 'Gas bill'}));
 store.dispatch(setTextFilter('water'));
+
+setTimeout(() => {
+    store.dispatch(setTextFilter('rent'))
+},3000);
 
 
 const state = store.getState();
@@ -23,5 +27,11 @@ console.log(visibleExpenses);
 
 console.log(store.getState());
 
-ReactDOM.render(<AppRouter/>, document.getElementById('app'));
+const jsx = (
+    <Provider store={store}>
+        <AppRouter/>
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
 
--- src/components/ExpenseDashboradPage.js
+++ src/components/ExpenseDashboradPage.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import ExpenseList from './ExpenseList'
 
 const ExpenseDashboradPage = () => (
     <div>
-        <p>This is from my dashboard component</p>
+        <ExpenseList/>
 
     </div>
 );
