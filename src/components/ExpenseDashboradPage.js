0dcd83994efbcd1eee97fe97d13dabd458660eb6
Pamela Szymaniec <pamela.szymaniec@gmail.com>
2018-07-24 13:29:00 +0200

    task 11/103 Controlled Inputs for Filters

--- src/components/ExpenseDashboradPage.js
+++ src/components/ExpenseDashboradPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ExpenseList from './ExpenseList'
+import ExpenseListFilters from './ExpenseListFilters';
 
 const ExpenseDashboradPage = () => (
     <div>
+        <ExpenseListFilters/>
         <ExpenseList/>
 
     </div>
