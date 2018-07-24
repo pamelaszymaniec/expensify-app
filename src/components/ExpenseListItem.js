544fe09b77ae82e09dc70f063c5f4ef0e8bdb7b4
Pamela Szymaniec <pamela.szymaniec@gmail.com>
2018-07-24 14:26:15 +0200

    task 11/104 Dropdown for picking SortBy

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -8,7 +8,7 @@ import { removeExpense } from '../actions/expenses'
 const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
     <div>
         <h3>{description}</h3>
-        <p>{amount} - {createdAt} - {id}</p>
+        <p>{amount} - {createdAt}</p>
         <button onClick={() => {
             dispatch(removeExpense( {id}));
         }}>Remove</button>
