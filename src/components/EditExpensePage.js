8afb2a159c48f57d72723d43a6211c5177b39cfc
Pamela Szymaniec <pamela.szymaniec@gmail.com>
2018-07-25 09:00:18 +0200

    task 11/109 redux dev tools, fixing errors

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -8,7 +8,7 @@ const EditExpensePage = (props) => {
         <ExpenseForm
             expense={props.expense}
             onSubmit={(expense) => {
-                props.dispatch(editExpense(expense));
+                props.dispatch(editExpense(props.expense.id, expense));
                 props.history.push('/')
             }}
         />
