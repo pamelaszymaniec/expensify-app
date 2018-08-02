5a2037a28a84a1b6da33d529d127caad41d1f0ef
Pamela Szymaniec <pamela.szymaniec@gmail.com>
2018-08-02 13:22:35 +0200

    webpack production setup

--- src/app.js
+++ src/app.js
@@ -7,14 +7,11 @@ import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
-
 import {addExpense} from './actions/expenses'
 import {setTextFilter} from './actions/filters'
 import getVisibleExpenses from './selectors/expenses'
 
-
 const store = configureStore();
-
 
 const jsx = (
     <Provider store={store}>
