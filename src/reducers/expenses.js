02f4a2d16f847c865176e6ea4d141e9a02c6ad3b
Pamela Szymaniec <pamela.szymaniec@gmail.com>
2018-08-10 16:20:10 +0200

    Get firebase expenses on start

--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -21,8 +21,12 @@ export default (state = expensesReducerDefaultState, action) => {
                 } else {
                     return expense;
                 }
-                ;
+
+
             });
+        case 'SET_EXPENSES':
+            return action.expenses;
+
         default:
             return state;
     }
